7064a130980f1d998f199c6b65f88e6a61649650
Oussamabenali4 <oussama.benali@ump.ac.ma>
2024-06-13 14:47:48 +0100

    using http.Handle to map function to paths

--- main.go
+++ main.go
@@ -5,29 +5,36 @@ import (
 	"net/http"
 )
 
-// what if we want to make my own hundler
-// any type that has the method ServeHTTP(http.ResponseWriter, *http.Request)
-// can be a handler
-// EX:
+// the handler we used previously handles all request with the same response
+// if we want to handle diffrent request with different responses
+// we need to make more handlers
+type Hello struct{}
 
-type Example struct{}
+func (E *Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello")
+}
 
-func (E *Example) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "this is an example")
+type World struct{}
+
+func (E *World) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "world")
 }
 
 func main() {
-	// we define a handler
-	// this hundler will respond to any request by the same thing
-	// "this is an example"
-	handler := Example{}
-	// we creat an http server struct
+	// we define a handler multip handler
+	hello := Hello{}
+	world := World{}
+	// in this case we use the default handler
+	// to take care of the methods used to handle
+	// different request
 	server := http.Server{
 		// 127.0.0.1 = localhost
-		Addr:    "127.0.0.1:8080",
-		Handler: &handler,
+		Addr: "127.0.0.1:8080",
 	}
 	// this code now will run and hundle any request
 	// with 404 not found
+	http.Handle("/hello", &hello)
+	http.Handle("/world", &world)
 	server.ListenAndServe()
 }
+// now visit localhost:8080/hello or localhost:8080/world on your browser 
